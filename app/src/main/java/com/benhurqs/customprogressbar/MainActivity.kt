e5265b1b7f46bdde8ac243d56ff3397ed59a0998
Benhur Souza <benhurqs@MacBook-de-Benhur.local>
2018-10-25 00:27:46 -0200

    Created method to change progress background

--- app/src/main/java/com/benhurqs/customprogressbar/MainActivity.kt
+++ app/src/main/java/com/benhurqs/customprogressbar/MainActivity.kt
@@ -5,23 +5,46 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import android.graphics.PorterDuff
+import android.graphics.drawable.ClipDrawable
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.LayerDrawable
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progress.progress = 50
+        edt_background.setText("#FF0000")
+        edt_progress.setText("#00FF00")
     }
 
-    fun onClicChangeProgressColor(view: View){
-
+    fun onClickChangeProgressColor(view: View){
+        changeProgressBG(
+            onColor = convertStringToColor(edt_progress.text.toString()),
+            offColor = convertStringToColor(edt_background.text.toString())
+        )
     }
-    
 
-    fun convertStringToColor(colorText: String): Int{
-        var color = Color.parseColor("#FFFFFF");
+    private fun changeProgressBG(onColor: Int, offColor: Int){
+        progress.progressDrawable = this.resources.getDrawable(R.drawable.custom_progressbar);
+        val layerDrawable = progress.progressDrawable as LayerDrawable
+
+        val bgGradientDrawable = layerDrawable
+            .findDrawableByLayerId(android.R.id.background) as GradientDrawable
+        bgGradientDrawable.setColor(offColor)
+        val progressGradientDrawable = layerDrawable
+            .findDrawableByLayerId(android.R.id.progress) as ClipDrawable
+        progressGradientDrawable.setColorFilter(onColor, PorterDuff.Mode.SRC_IN)
+    }
+
+    private fun convertStringToColor(colorText: String): Int{
+        var color = Color.parseColor("#FFFFFF")
         try{
-            color = Color.parseColor(colorText);
+            color = Color.parseColor(colorText)
         }catch (e: Exception){
             Toast.makeText(this, "Invalid color", Toast.LENGTH_SHORT).show()
         }
